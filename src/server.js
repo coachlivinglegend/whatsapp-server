d5c4df2897125a8d0db9496e2d2901718c5df89f
Daniel Beckley <tomiwabeckley@gmail.com>
2020-09-27 16:48:41 +0100

    the cors

--- src/server.js
+++ src/server.js
@@ -26,12 +26,12 @@ app.use(express.json())
 
 const router = express.Router();
 
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-//     next();
-// })
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+})
 
 
 //routes
